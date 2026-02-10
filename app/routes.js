7b0fb03a5548c23fefe8807adc040aed4be440f6
agz deberny gomes <agnieszka.deberny-gomes@digital.cabinet-office.gov.uk>
2026-02-10 15:07:04 +0000

    changes to cookie banner
    changes to cookie banner
    
    A cheeky way to change the message on the banner depending on the choice. Show message after the user accepts or rejects the cookies.
    
    and hide the banner when the user chooses hide button.
    
    There's defo a better way to do it, but it should be good enough for now

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,51 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+
+// Show message on the cookie banner after the user has submitted accept or reject
+router.post('/pages/cookie-banner', function (req, res) {
+
+    var cookiePreference = req.session.data['cookies']
+
+    if (cookiePreference == 'no') {
+
+        res.redirect('/pages/cookie-banner-reject')
+
+    } else {
+
+    res.render('/pages/cookie-banner-accept')
+    } 
+
+})
+
+// "close the banner"
+router.post('/pages/banner-accept', function (req, res) {
+
+    var hideBanner = req.session.data['hide']
+
+    if (hideBanner == 'yes') {
+
+        res.redirect('/pages/welcome')
+
+    } else {
+
+    res.render('/pages/cookie-banner-accept')
+    } 
+
+})
+
+// "close the banner"
+router.post('/pages/banner-reject', function (req, res) {
+
+    var hideBanner = req.session.data['hide']
+
+    if (hideBanner == 'yes') {
+
+        res.redirect('/pages/welcome')
+
+    } else {
+
+    res.render('/pages/cookie-banner-reject')
+    } 
+
+})
